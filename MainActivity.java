69118ff69718601de5cd4a298d8e6a23da8afa8c
mirva16 <72028077+mirva16@users.noreply.github.com>
2021-08-27 16:21:47 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -2,13 +2,41 @@ package com.example.quiz_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    EditText mTextUsername;
+    EditText mTextPassoword;
+    Button mButtonLogin;
+    TextView mTextViewRegister;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextUsername = (EditText) findViewById(R.id.edittext_username);
+        mTextPassoword = (EditText) findViewById(R.id.edittext_password);
+        mButtonLogin = (Button) findViewById(R.id.button_login);
+        mTextViewRegister = (TextView) findViewById(R.id.textview_register);
+        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent registerIntent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+
+
+
     }
 }
